44682e1ea534eecbd031772ca30837227b63fa00
Viniciusdacostasilva <viniciusdacostasilva95@gmail.com>
2024-06-20 14:59:37 -0300

    ajuste de visuais

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -7,7 +7,7 @@ function AboutUsSection({id}:any) {
   return (
     <div id="about-us" className="about-us-section">
       <h2>Sobre nós</h2>
-      <img src="https://wow.zamimg.com/uploads/guide/seo/4672.jpg?1478722534" alt="Imagem Sobre Nós" />
+      <img src="https://wow.zamimg.com/uploads/guide/seo/4672.jpg?1478722534" alt="Imagem Sobre Nós" style={{margin:"20px auto"}} />
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla lacinia ex nec magna molestie, id commodo nulla varius.</p>
     </div>
   );

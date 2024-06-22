95b99f7f55c7110c4b88aa4e1464ecd5969a5005
Viniciusdacostasilva <viniciusdacostasilva95@gmail.com>
2024-06-22 11:55:09 -0300

    config de imgs para arquivos locais e ajustes

--- src/components/AboutUsSection.tsx
+++ src/components/AboutUsSection.tsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import '../styles/AboutUsSection.css';
+import Image from 'next/image';
+import aboutImg from '../img/about.jpg'; // Certifique-se de que o caminho está correto
 
 function AboutUsSection({id}:any) {
   return (
     <div id="about-us" className="about-us-section">
       <h2>Sobre nós</h2>
-      <img src="https://wow.zamimg.com/uploads/guide/seo/4672.jpg?1478722534" alt="Imagem Sobre Nós" style={{margin:"20px auto"}} />
+      <Image src={aboutImg} alt="Imagem Sobre Nós" style={{margin:"20px auto"}} />
       <p>Somos uma equipe dedicada de aventureiros virtuais apaixonados pelo desafio e pela excelência. No coração de Azeroth, nos destacamos não apenas pelo nosso desempenho excepcional, mas também pelo compromisso com a qualidade e a satisfação do cliente. Com anos de experiência em World of Warcraft, oferecemos serviços de boost de M+ que são tanto eficientes quanto acessíveis, garantindo que você possa alcançar suas metas no jogo sem complicações.</p>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@
 import React, { useState } from 'react';
 import { FaArrowUp, FaWhatsapp, FaDiscord, FaBars, FaTimes } from 'react-icons/fa';
 import '../styles/Header.css';
+import Image from 'next/image';
+import endImg from '../img/end.png'; // Certifique-se de que o caminho está correto
 
 function Header({ visible }: any) {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -29,7 +31,7 @@ function Header({ visible }: any) {
       <div className={visible ? 'header' : 'header hidden'}>
         <div className="header-container">
           <div className="header-left">
-            <img src="https://cdn.discordapp.com/attachments/928007050439561256/1253507392658870374/end_gay_1.png?ex=6676c3fb&is=6675727b&hm=f23ce6c33ef8dce31c89fd6b956c2c37e0360e7503978aaaee83bfdbb0d6baa6&" alt="End Gear Icon" className="header-icon" />
+          <Image src={endImg} alt="End Gear" className='header-icon'/>
             <h1 className="header-title">End Gear</h1>
           </div>
           <div className="header-right">
